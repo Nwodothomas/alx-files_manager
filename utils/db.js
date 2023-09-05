33cb57eb633e1ba03c0f5792016ac60e2370346f
Nwodothomas <108361891+Nwodothomas@users.noreply.github.com>
2023-09-06 00:39:01 +0100

    added db.js

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const { MongoClient } = require('mongodb');
-const mongo = require('mongodb');
 
 class DBClient {
   constructor() {
+    // Set default values for connection parameters if environment variables are not defined
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     this.database = process.env.DB_DATABASE || 'files_manager';
