d3d2fea1d9d087470af3279a86132687fb40c845
Nwodothomas <108361891+Nwodothomas@users.noreply.github.com>
2023-09-06 01:03:55 +0100

    added db.js

--- utils/db.js
+++ utils/db.js
@@ -1,17 +1,18 @@
-#!/usr/bin/node
-
 const { MongoClient } = require('mongodb');
 const mongo = require('mongodb');
-const { pwdHashed } = require('./utils');
 
 class DBClient {
   constructor() {
-    const host = (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost';
-    const port = (process.env.DB_PORT) ? process.env.DB_PORT : 27017;
-    this.database = (process.env.DB_DATABASE) ? process.env.DB_DATABASE : 'files_manager';
+    // Default values if environment variables are not set
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
     const dbUrl = `mongodb://${host}:${port}`;
+
     this.connected = false;
     this.client = new MongoClient(dbUrl, { useUnifiedTopology: true });
+
     this.client.connect().then(() => {
       this.connected = true;
     }).catch((err) => console.log(err.message));
