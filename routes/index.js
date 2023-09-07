4a41a1edfa65100a1c1dca952951ff9c107a5c72
Nwodothomas <108361891+Nwodothomas@users.noreply.github.com>
2023-09-07 22:22:30 +0100

    updated index.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 
-import { Router } from 'express';
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
-const router = Router();
+const router = express.Router();
 
-// Task 2 - First API
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
-// Task 3 - Create a new user
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', AuthController.getMe);
 
-export default router;
+module.exports = router;
