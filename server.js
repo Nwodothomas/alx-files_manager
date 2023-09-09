ed898c6cf99495ef5f35ca98dd9603324ca86bb9
Nwodo Thomas <Nwodothomas@gmail.com>
2023-09-10 00:54:43 +0100

    added server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 
-const express = require('express');
-const routes = require('./routes/index');
+const express = require("express");
+const router = require("./routes/index");
 
 const server = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 server.use(express.json());
-app.use(routes);
+server.use(router);
 
-
-
-server.listen(PORT, () => {
+server.listen(PORT, () =>
   console.log(`Server running on port: ${PORT}`)
-});
+);
