6f111c372fba0af790362ea891ea12245e47f3d2
Nwodothomas <108361891+Nwodothomas@users.noreply.github.com>
2023-09-11 00:14:32 +0100

    added server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-#!/usr/bin/node
-
 const express = require("express");
 const router = require("./routes/index");
 
-const app = express();
+const server = express();
 const PORT = process.env.PORT || 5000;
 
 server.use(express.json());
