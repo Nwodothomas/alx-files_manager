7854a7ef7d12f2152a885acde77d7a2152df0a36
Nwodo Thomas <Nwodothomas@gmail.com>
2023-09-10 01:47:57 +0100

    added AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,28 +4,18 @@ const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
 class AppController {
-  static async getStatus(req, res) {
-    const redisStatus = await redisClient.isAlive();
-    const dbStatus = await dbClient.isAlive();
-
-    const response = {
-      redis: redisStatus,
-      db: dbStatus,
-    };
-
-    return res.status(200).json(response);
+  static getStatus(req, res) {
+    if (redisClient.isAlive() && dbClient.isAlive()) {
+      res.json({ redis: true, db: true });
+      res.end();
+    }
   }
 
   static async getStats(req, res) {
-    const usersCount = await dbClient.nbUsers();
-    const filesCount = await dbClient.nbFiles();
-
-    const response = {
-      users: usersCount,
-      files: filesCount,
-    };
-
-    return res.status(200).json(response);
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.json({ users, files });
+    res.end();
   }
 }
 
