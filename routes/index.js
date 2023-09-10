ea0e37c775af13928d4fe7313f0e7a68dd29660b
Nwodo Thomas <Nwodothomas@gmail.com>
2023-09-10 01:33:08 +0100

    added index.js

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const AppController = require("../controllers/AppController");
 
 const router = express.Router();
 
+// Define routes and link them to controller methods
 router.get("/status", AppController.getStatus);
 router.get("/stats", AppController.getStats);
 
