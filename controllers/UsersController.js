8de92dfe6b79081d57d4e60039f6ee6e4e0b2a64
Nwodo Thomas <Nwodothomas@gmail.com>
2023-09-11 09:35:39 +0100

    added UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,23 +1,37 @@
 #!/usr/bin/node
 
-const dbClient = require('../utils/db');
+const sha1 = require('sha1');
+const DBClient = require('../utils/db');
 
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
+
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
     }
+
     if (!password) {
       return res.status(400).json({ error: 'Missing password' });
     }
-    const userExist = await dbClient.userExist(email);
-    if (userExist) {
-      return res.status(400).json({ error: 'Already exists' });
+
+    const userExists = await DBClient.userExist(email);
+
+    if (userExists) {
+      return res.status(400).json({ error: 'Already exist' });
     }
-    const user = await dbClient.createUser(email, password);
-    const id = `${user.insertedId}`;
-    return res.status(201).json({ id, email });
+
+    const hashedPassword = sha1(password);
+
+    try {
+      const newUser = await DBClient.createUser(email, hashedPassword);
+      return res.status(201).json({
+        id: newUser.insertedId,
+        email,
+      });
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 }
 
